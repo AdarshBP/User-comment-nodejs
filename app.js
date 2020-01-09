867c0f15815662db048498d5b79ddd334017547a
AdarshBP <bpadarsh8@gmail.com>
2020-01-09 05:33:31 +0530

    started coding

---
+++ app.js
@@ -0,0 +1,32 @@
+const http=require('http');
+const express=require('express');
+const bodyParser = require('body-parser');
+const path =require('path');
+
+var app=express();
+app.set('view engine','ejs');
+var data=[];
+app.use(bodyParser.urlencoded({extend:false}));
+app.get('/',(req,res)=>
+{ 
+    res.sendFile(path.join(__dirname,'views','comment.html'));
+});
+
+app.post('/submit',(req,res)=>
+{
+    var today = new Date().toLocaleDateString();
+    data.push({
+            date:today,
+            name:req.body.comment_name,
+            comment:req.body.comment_content
+    });
+    console.log(data);
+    res.redirect('/');
+});
+
+app.get('/CommentSection',(req,res)=>{
+    let allcommentspath=path.join(__dirname,'views','allComments.ejs');
+    res.render(allcommentspath,{data:data});
+});
+//listing to port 3000
+app.listen(3000);

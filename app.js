db04612b6734aa4b8623ffb8d29b998017ff09ea
AdarshBP <bpadarsh8@gmail.com>
2020-11-13 16:32:23 +0530

    aded telegram support

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http=require('http');
 const express=require('express');
 const bodyParser = require('body-parser');
 const path =require('path');
+var TelegramBot = require('node-telegram-bot-api'),
+// Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+bot = new TelegramBot("1435684552:AAGaVCohGoeo6iNb0WY6E96dT2-RWN0VYfA", { polling: true });
+
 
 var app=express();
 app.set('port',(process.env.PORT || 8000))
@@ -21,8 +25,10 @@ app.post('/submit',(req,res)=>
             name:req.body.comment_name,
             comment:req.body.comment_content
     });
-    console.log(data);
-    res.redirect('https://designers-group.herokuapp.com/index.html');
+    console.log("asd"+data);
+    bot.sendMessage(234437958,"\n#"+ req.body.comment_name + "\n#"+ req.body.comment_content + "\n" + today);
+    //res.redirect('https://designers-group.herokuapp.com/index.html');
+    res.redirect('/');
 });
 
 app.get('/CommentSection',(req,res)=>{
@@ -33,3 +39,8 @@ app.get('/CommentSection',(req,res)=>{
 app.listen(app.get('port'),()=>{
     console.log('Node app is uprunning om port',app.get('port'));
 });
+
+bot.on('message', (msg) => {
+    console.log(msg.chat.id);
+    bot.sendMessage(msg.chat.id, 'Ill have the tuna. No crust.');
+  });

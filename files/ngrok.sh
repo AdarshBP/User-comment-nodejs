5c3224880e34f2ad47a243e5c5fa02ae95566ebd
Adarsh BP <36394852+AdarshBP@users.noreply.github.com>
2021-08-06 08:34:50 +0530

    Updated auth
    Updated auth

--- files/ngrok.sh
+++ files/ngrok.sh
@@ -5,5 +5,6 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip;
 unzip ngrok-stable-linux-arm.zip;
 chmod +x ngrok;
 ./ngrok authtoken 51jGsRuaFyyw3bnhpMmsB_Mf7Z34SpV5GvgDgMTT1x;
+
 curl -G 'https://comment-management.herokuapp.com/message' --data-urlencode message="ngrok completed";
 ./ngrok tcp 8022
